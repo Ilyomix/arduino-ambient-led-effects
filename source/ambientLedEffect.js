e6edc38004c91b2774e9027bfb1ba09753812815
Ilyes Abd-Lillah <i.abdlillah@inovans.fr>
2019-09-21 15:26:34 +0200

    [0.0.1b1] : POC led effect on AdaLed strip

---
+++ source/ambientLedEffect.js
@@ -0,0 +1,24 @@
+pixel = require("node-pixel");
+five = require("johnny-five");
+ 
+var opts = {};
+var board = new five.Board(opts);
+var strip = null;
+ 
+board.on("ready", function() {
+ 
+    strip = new pixel.Strip({
+        board: this,
+        controller: "FIRMATA",
+        strips: [ {pin: 6, length: 60}, ], // this is preferred form for definition
+        gamma: 2.8, // set to a gamma that works nicely for WS2812
+    });
+ 
+    strip.on("ready", function() {
+        strip.pixel(0).color("#000");
+        strip.pixel(1).color("red");
+        strip.shift(1, pixel.FORWARD, true);
+        strip.pixel(1).color; // will now be nothing
+        strip.pixel(2).color; // will now be red.
+    });
+});
